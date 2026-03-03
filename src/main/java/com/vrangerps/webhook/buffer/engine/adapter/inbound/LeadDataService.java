ce5caf0063a5de3b93a2c17a3e528b9d43b9e516
vrangerps <amandvivek.ananda@gmail.com>
2026-03-03 23:57:08 +0530

    feat(adaptor): update lead data service (Closes#21)

--- src/main/java/com/vrangerps/webhook/buffer/engine/adapter/inbound/LeadDataService.java
+++ src/main/java/com/vrangerps/webhook/buffer/engine/adapter/inbound/LeadDataService.java
@@ -3,6 +3,8 @@ package com.vrangerps.webhook.buffer.engine.adapter.inbound;
 import com.vrangerps.webhook.buffer.engine.domain.model.LeadData;
 import com.vrangerps.webhook.buffer.engine.ports.inbound.LeadWebhookProcessor;
 import com.vrangerps.webhook.buffer.engine.ports.inbound.LeadWebhookReceiver;
+import com.vrangerps.webhook.buffer.engine.ports.outbound.LeadEventPublisher;
+import com.vrangerps.webhook.buffer.engine.ports.outbound.LeadRepository;
 
 import java.util.List;
 
@@ -10,14 +12,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LeadDataService implements LeadWebhookReceiver, LeadWebhookProcessor {
+
+    private final LeadEventPublisher leadEventPublisher;
+    private final LeadRepository leadRepository;
+
+    public LeadDataService(LeadEventPublisher leadEventPublisher, LeadRepository leadRepository) {
+        this.leadEventPublisher = leadEventPublisher;
+        this.leadRepository = leadRepository;
+    }
     
     @Override
     public void receiveLeadEvent(List<LeadData> leadDataList) {
-
+        for (LeadData leadData : leadDataList) {
+            leadEventPublisher.publishLeadEvent(leadData);   
+        }
     }
 
     @Override
     public void processLeadEvent(LeadData leadData) {
-        
+        leadRepository.findMatchingLeads(leadData);
+        leadRepository.saveLead(leadData);
     }
 }
