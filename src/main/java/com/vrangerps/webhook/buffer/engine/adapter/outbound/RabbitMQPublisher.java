6555e2409fd99a2cd08a62f335db7e0b177549d2
vrangerps <amandvivek.ananda@gmail.com>
2026-03-05 23:19:24 +0530

    refactor(config): updat config properties from resources file to config class (Closes #11)

--- src/main/java/com/vrangerps/webhook/buffer/engine/adapter/outbound/RabbitMQPublisher.java
+++ src/main/java/com/vrangerps/webhook/buffer/engine/adapter/outbound/RabbitMQPublisher.java
@@ -1,30 +1,22 @@
 package com.vrangerps.webhook.buffer.engine.adapter.outbound;
 
+import com.vrangerps.webhook.buffer.engine.config.RabbitMQConfig;
 import com.vrangerps.webhook.buffer.engine.domain.model.LeadData;
 import com.vrangerps.webhook.buffer.engine.ports.outbound.LeadEventPublisher;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQPublisher implements LeadEventPublisher {
 
-    @Value("${rabbitmq.exchange.name}")
-    private String exchangeName;
-
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
-
     private RabbitTemplate rabbitTemplate;
 
-    @Autowired
     public RabbitMQPublisher(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
     public void publishLeadEvent(LeadData leadData) {
-        rabbitTemplate.convertAndSend(exchangeName, routingKey, leadData.toString());
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, leadData.toString());
     }
 }
